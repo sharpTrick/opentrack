ce02a52adc1eea16280e4b6efac7a64113df1641
Stanislaw Halik <sthalik@misaki.pl>
2021-12-19 16:46:21 +0100

    trackhat: move defs to dedicated file

--- opentrack/defs.hpp
+++ opentrack/defs.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-//#define UI_FORCED_TRACKER "pt"
-//#define UI_FORCED_FILTER "accela"
-
-//#define UI_NO_TRACKER_COMBOBOX
-//#define UI_NO_FILTER_COMBOBOX
-
-//#define UI_NO_TRACKER_SETTINGS_BUTTON
-//#define UI_NO_FILTER_SETTINGS_BUTTON
-
-//#define UI_NO_RAW_DATA
-//#define UI_NO_GAME_DATA
+#define UI_FORCED_TRACKER "trackhat"
+#define UI_FORCED_FILTER "accela"
+#define UI_NO_TRACKER_COMBOBOX
+#define UI_NO_FILTER_COMBOBOX
+#define UI_NO_TRACKER_SETTINGS_BUTTON
+#define UI_NO_FILTER_SETTINGS_BUTTON
+#define UI_NO_RAW_DATA
+#define UI_NO_GAME_DATA
